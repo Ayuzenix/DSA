a4ebc9839ee0605ad11ab3eab9b94fbff6e207ac
Ayuzenix <bt25cse042@nituk.ac.in>
2026-01-28 16:42:30 +0530

    Add Best Time to Buy and Sell Stock

---
+++ Arrays/Best_Time_To_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem:Best Time to Buy and Sell Stock
+// Platform:Leetcode
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int mini=INT_MAX;
+        int profit=0;
+        for(int i=0;i<n;i++){
+            mini=min(mini,prices[i]);
+            profit=max(profit,prices[i]-mini);
+        }
+        return profit;
+    }
+};
