dbdc86f192cc6f542b078ade36fecd25548b4d23
Ayuzenix <bt25cse042@nituk.ac.in>
2026-01-28 11:32:48 +0530

    Add sort colors array solution

---
+++ sort_colores.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+// problem:sort colors;
+// platform:leetcode
+// approach: brute 
+// expected: dutch national flag algo (need to learn this)
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int red=0;
+        int white=0;
+        int blue=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                red++;
+            }
+            if(nums[i]==1){
+                white++;
+            }
+            if(nums[i]==2){
+                blue++;
+            }
+        }
+        for(int i=0;i<red;i++){
+            nums[i]=0;
+        }
+        for(int i=red;i<red+white;i++){
+            nums[i]=1;
+        }
+        for(int i=red+white;i<red+white+blue;i++){
+            nums[i]=2;
+        }
+        return ;
+    }
+};
