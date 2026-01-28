3d3c5a8d0dd056b68109c833c281d04ae10cec12
Ayuzenix <bt25cse042@nituk.ac.in>
2026-01-28 18:43:49 +0530

    Add Rotated Sorted Array by Binary Search Solution

---
+++ Binary_Search/Find_mininum_in_rotated_sorted_array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem:Find Minimun in Rotated Sorted Array
+// Platform:Leetcode
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int mini=INT_MAX;
+        while(low<=high){
+            int mid=(low+high)/2;
+            mini=min(mini,nums[mid]);
+            if(nums[low]<=nums[mid]){
+                if(nums[mid]>nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }else{
+                if(nums[mid]<nums[low]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+            }
+        }
+        return mini;
+    }
+};
