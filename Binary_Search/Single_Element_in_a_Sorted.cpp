0379dfaec92dec54f4d25096454bf84d9fe1ae6b
Ayuzenix <bt25cse042@nituk.ac.in>
2026-01-28 19:13:05 +0530

    Single Elements in a sorted array Binary Search solution

---
+++ Binary_Search/Single_Element_in_a_Sorted.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Problem:Single Element in a Sorted Array
+// Platform:Leetcode
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1){
+            return nums[0];
+        }
+        if(nums[0]!=nums[1]){
+            return nums[0];
+        }
+        if(nums[n-1]!=nums[n-2]){
+            return nums[n-1];
+        }
+        //Now the search space that we left with is between from 1 index to n-2
+        int low=1;
+        int high=n-2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            if(mid%2==0 && nums[mid]==nums[mid-1] ||
+               mid%2!=0 && nums[mid]==nums[mid+1] ){
+                high=mid-1;
+            }else if(mid%2!=0 && nums[mid]==nums[mid-1] ||
+                     mid%2==0 && nums[mid]==nums[mid+1] ){
+                low=mid+1;
+              }
+        }
+        return -1;
+    }
+};
