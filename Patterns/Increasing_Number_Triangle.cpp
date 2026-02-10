adc5cdc780adb9eb9bde4f7de0501f9ca154ebe0
Ayuzenix <bt25cse042@nituk.ac.in>
2026-02-10 18:30:12 +0530

    Increasing Numbers Triangle Solution

---
+++ Patterns/Increasing_Number_Triangle.cpp
@@ -0,0 +1,24 @@
+/*
+========================================
+Pattern Name   : Increasing Numbers Triangle
+
+1
+1 2
+1 2 3
+1 2 3 4
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+            cout << j + 1 << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
