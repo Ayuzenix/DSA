0271fbb654cb924be300a0fb6b5ff85dc278541f
Ayuzenix <bt25cse042@nituk.ac.in>
2026-03-25 19:53:37 +0530

    Add Sum of Beauty of all Substrings String Solution

--- Strings/Sum_of_Beauty_of_all_Subarray.cpp
+++ Strings/Sum_of_Beauty_of_all_Subarray.cpp
@@ -1,27 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// Problem:Sum of Beauty of all Subarray
+// Problem:Sum of Beauty of all Substrings
 // Platform:Leetcode
 
+// Solution 1-Without using Map
+class Solution {
+public:
+    int totalCount(string &curr){
+        vector<int>store(26,0);
+        int max_freq=INT_MIN;
+        int min_freq=INT_MAX;
+        for(int i=0;i<curr.size();i++){
+            store[curr[i]-'a']++;
+        }
+        for(int i=0;i<store.size();i++){
+                max_freq=max(max_freq,store[i]);
+                if(store[i]!=0){
+                min_freq=min(min_freq,store[i]);
+                }
+        }
+        if(min_freq==INT_MAX){
+            min_freq=0;
+        }
+        return (max_freq-min_freq);
+    }
+    int beautySum(string s) {
+        int n=s.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            string curr="";
+            for(int j=i;j<n;j++){
+                curr=curr+s[j];
+                sum=sum+totalCount(curr);
+            }
+        }
+        return sum;
+    }
+};
+
+// Solution 2-By using map
 class Solution {
 public:
     int beautySum(string s) {
         int n=s.size();
         int sum=0;
         for(int i=0;i<n;i++){
-            vector<int>store(26,0);
+            unordered_map<int,int>mp;
             for(int j=i;j<n;j++){
-                store[s[j]-'a']++;
-                int maxi=INT_MIN;
-                int mini=INT_MAX;
-                for(int k=0;k<26;k++){
-                    if(store[k]>0){
-                        maxi=max(maxi,store[k]);
-                        mini=min(mini,store[k]);
-                    }
+                mp[s[j]-'a']++;
+                int max_freq=INT_MIN;
+                int min_freq=INT_MAX;
+                for(auto &it:mp){
+                    max_freq=max(max_freq,it.second);
+                    min_freq=min(min_freq,it.second);
                 }
-                sum=sum+(maxi-mini);
+                sum=sum+(max_freq-min_freq);
             }
         }
         return sum;
