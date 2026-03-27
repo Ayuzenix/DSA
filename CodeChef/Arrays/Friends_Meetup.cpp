f6449a8a067c49feedec7508b152ff5d9d525a01
Ayuzenix <bt25cse042@nituk.ac.in>
2026-03-27 17:27:54 +0530

    Add Friends Meetup Codechef Solution

---
+++ CodeChef/Arrays/Friends_Meetup.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem:Friends Meetup Practice
+// Platform:CodeChef
+// Rating:1098
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(y==x){
+	      cout<<"YES"<<endl;
+	    }else if(x<y){
+	        cout<<"NO"<<endl;
+	    }else {
+	        cout<<"YES"<<endl;
+	    }
+	    
+	}
+   return 0;
+}
