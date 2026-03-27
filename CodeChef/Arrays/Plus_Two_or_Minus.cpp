e031cc49ac693019bd72097e4759f5c1c4bb7bb5
Ayuzenix <bt25cse042@nituk.ac.in>
2026-03-27 18:10:37 +0530

    Add Plus Two or Minus Array CodeChef Solution

---
+++ CodeChef/Arrays/Plus_Two_or_Minus.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem:Plus Two of Minus
+// Platform:CodeChef
+// Rating:1098
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int y;
+	    cin>>y;
+	    int ans;
+	    if(y==0){
+	        cout<<"1"<<endl;
+	    }else {
+	        ans=2*y+y;
+	        cout<<ans<<endl;
+	    }
+	    
+	}
+	return 0;
+}
