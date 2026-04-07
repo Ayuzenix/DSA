703529ebeedad15d68e81940811ff8765bbb671e
Ayuzenix <bt25cse042@nituk.ac.in>
2026-04-07 12:31:33 +0530

    Update Minimum_flip_to_make_a_OR_b_equal_to_c.cpp

--- Bit_Manipulation/Minimum_flip_to_make_a_OR_b_equal_to_c.cpp
+++ Bit_Manipulation/Minimum_flip_to_make_a_OR_b_equal_to_c.cpp
@@ -30,3 +30,29 @@ public:
         return count ; 
     }
 };
+
+// Second Approach --> Eleminating that extra variables taken 
+ 
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int count = 0 ;
+        for ( int i = 0 ; i < 32 ; i ++ ) {
+             int x = ( a & ( 1 << i ) ) ;
+             int y = ( b & ( 1 << i ) ) ;
+             int z = ( c & ( 1 << i ) ) ;
+                if ( z == 0 ) {
+                     if ( ( x == 0 && y != 0 ) || ( x != 0 && y == 0 ) ) {
+                        count = count + 1 ;
+                     } else if ( ( x != 0 && y != 0 ) ) {
+                        count = count + 2 ;
+                     }
+                } else if ( z != 0 ) {
+                     if ( ( x == 0 && y == 0 ) ) {
+                        count = count + 1 ;
+                     } 
+                }
+        }
+        return count ; 
+    }
+};
