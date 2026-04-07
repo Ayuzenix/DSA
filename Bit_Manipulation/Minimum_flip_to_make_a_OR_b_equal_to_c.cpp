1ec5580acef317bc4c3b20f97b0af15c1aa0ac93
Ayuzenix <bt25cse042@nituk.ac.in>
2026-04-07 12:28:47 +0530

    Add Minimum flip to make a OR b equal to c Bit Manipulation Solution

---
+++ Bit_Manipulation/Minimum_flip_to_make_a_OR_b_equal_to_c.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+// Problem : Minimum flip to make a OR b equal to c 
+// Platform : Leetcode 
+
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int count = 0 ;
+        int maxi1 = max ( a , b ) ;
+        int maxi = max ( maxi1 , c ) ;
+        int n = log2( maxi ) + 1 ;
+        for ( int i = 0 ; i < n ; i ++ ) {
+             int x = ( a & ( 1 << i ) ) ;
+             int y = ( b & ( 1 << i ) ) ;
+             int z = ( c & ( 1 << i ) ) ;
+                if ( z == 0 ) {
+                     if ( ( x == 0 && y != 0 ) || ( x != 0 && y == 0 ) ) {
+                        count = count + 1 ;
+                     } else if ( ( x != 0 && y != 0 ) ) {
+                        count = count + 2 ;
+                     }
+                } else if ( z != 0 ) {
+                     if ( ( x == 0 && y == 0 ) ) {
+                        count = count + 1 ;
+                     } 
+                }
+        }
+        return count ; 
+    }
+};
