839a7e62987889517099f8130dc4096317cf7cb3
Ayuzenix <bt25cse042@nituk.ac.in>
2026-06-01 19:52:43 +0530

    Add Ichihime and Triangle ( Problem 1337A )  Codeforces Solution

---
+++ CP/CodeForces/Problem_1337A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+// Problem : Ichihime and Triangle ( Problem 1337A ) 
+// Platform : Codeforces 
+
+int main() {
+    
+    int t ;
+    cin>>t ;
+    while ( t -- ) {
+          int a , b , c , d ;
+          cin>>a>>b>>c>>d ;
+          vector<int>result ;
+          cout<<b<<" "<<c<<" "<<c<<endl ;
+    }
+   return 0 ;           
+}
