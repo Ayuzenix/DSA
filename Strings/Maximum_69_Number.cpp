4dd819067fe4f467579302c417a7fa57daa4a5cb
Ayuzenix <bt25cse042@nituk.ac.in>
2026-06-10 10:14:38 +0530

    Add Maximum 69 Number Leetcode Solution

---
+++ Strings/Maximum_69_Number.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+// Problem : Maximum 69 Number 
+// Platform : Leetcode 
+
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string str = to_string(num) ;
+        for ( int i = 0 ; i < str.size() ; i ++ ) {
+            if ( str[i] =='6' ) {
+                str[i] = '9' ;
+                break ;
+            }
+        }
+        return stoi( str ) ;
+    }
+};
