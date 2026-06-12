b1bd8977f95e260568f0c9d4080cfccd32744fdb
Ayuzenix <bt25cse042@nituk.ac.in>
2026-06-12 09:48:12 +0530

    Add Construct the Minimum Bitwise Array I Leetcode Solution

---
+++ Bit_Manipulation/Construct_the_Minimum_Bitwise_Array_I.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+// Problem : Construct the Minimum Bitwise Array I 
+// Platform : Leetcode 
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+         int n = nums.size() ;
+         vector<int>result( n ) ;
+         for ( int i = 0 ; i < n ; i ++ ) { 
+              int x = nums[i] , curr = -1 ;
+              for ( int j = 0 ; j < ( log2(x) + 1 ) ; j ++ ) {
+                 int m = ( x & ( 1 << j ) ) ;
+                 if ( m == 0 ) {
+                    break ;
+                 }
+                 curr = ( x ^ ( 1 << j ) ) ;
+              }
+              result[i] = curr ;
+         }
+        return result ;
+    }
+};
