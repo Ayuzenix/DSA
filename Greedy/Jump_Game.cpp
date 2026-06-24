d4e5deca273ef4306f9643e92fb35707b82d5630
Ayuzenix <bt25cse042@nituk.ac.in>
2026-06-24 19:04:18 +0530

    Add Jump Game Leetcode Solution

---
+++ Greedy/Jump_Game.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+// Problem : Jump Game 
+// Platoform : Leetcode 
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size() ;
+        int maxReachable = 0  ;
+        for ( int i = 0 ; i < n ; i ++ ) {
+            if ( i > maxReachable ) {
+                return false ;
+            }
+            maxReachable = max( maxReachable , i + nums[i] ) ;
+        }
+        return true ;
+    }
+};
