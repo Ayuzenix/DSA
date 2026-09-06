1a5892e6424cc0b0b6c077d021b37cfbda476880
Ayuzenix <bt25cse042@nituk.ac.in>
2026-09-06 22:17:32 +0530

    Add Last Stone Weight Leetcode Solution

---
+++ Heap/Last_Stone_Weight.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+// Problem : Last Stone Weight 
+// Platform : Leetcode 
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size() ;
+        if ( n == 1 ) {
+            return stones[0] ;
+        }
+        priority_queue<int>pq ;
+        for ( int i = 0 ; i < n ; i ++ ) {
+             pq.push( stones[i] ) ;
+        }
+        while ( pq.size() > 1 ) {
+             int y = pq.top() ;
+             pq.pop() ;
+             int x = pq.top() ;
+             pq.pop() ;
+             if ( x < y ) {
+                 pq.push( y - x ) ;
+             }
+        }
+        if ( pq.empty() ) {
+             return 0 ;
+        }
+        return pq.top() ;
+    }
+};
